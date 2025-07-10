81bc8842f432340b84a6babd301608cb48b1aab4
AnishUdupaN <anish@email>
2025-07-10 17:02:56 +0530

    wip

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import multi
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent, PreferencesEvent, PreferencesUpdateEvent,ExtensionCustomAction
+from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent, PreferencesEvent, PreferencesUpdateEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.RunScriptAction import RunScriptAction
@@ -14,7 +14,6 @@ class ClipboardHistoryExtension(Extension):
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
         self.subscribe(PreferencesEvent, PreferencesEventListener())
         self.subscribe(PreferencesUpdateEvent, PreferencesUpdateEventListener())
-        self.subscribe(ExtensionCustomAction, CustomActionEventListener())
 
 
 class PreferencesEventListener(EventListener):
@@ -58,23 +57,7 @@ class KeywordQueryEventListener(EventListener):
         return RenderResultListAction(items[:num_entries])
         """
     def on_event(self, event, extension):
-        query = event.get_argument() or ""
-        if not query:
-            return RenderResultListAction([])
-
-        # Return one item prompting user to press enter to search
-        item = ExtensionResultItem(
-            icon='images/icon.png',
-            name=f"Press Enter to search for '{query}'",
-            description="Run the actual search",
-            on_enter=ExtensionCustomAction({"query": query})
-        )
-        return RenderResultListAction([item])
-
-class CustomActionEventListener(EventListener):
-    def on_event(self, event, extension):
         items = []
-        """
         stringinput = event.get_argument() or ""
         args = stringinput.strip().split(' in ')
         filename=args[0]
@@ -93,13 +76,6 @@ class CustomActionEventListener(EventListener):
                 description="Click to Open",
                 on_enter=RunScriptAction(f'xdg-open "{lines[i]}"', [])
             ))
-        """
-        items.append(ExtensionResultItem(
-            icon=os.path.join(os.getcwd(),'images/icon.png'),
-            name='hello',
-            description="Click to Open",
-            on_enter=RunScriptAction(f'xdg-open "{lines[i]}"', [])
-        ))
         num_entries = int(extension.preferences.get('num_entries', 10))
         return RenderResultListAction(items[:num_entries])
 
